b5ce68504a0103d3c6de279a01e08e01619d32ef
Elisa <elisadiazartcontact@gmail.com>
2023-03-01 18:58:52 +0100

    1.03.23

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,33 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
-import { sayHello } from './demo.js';
 
-import catImage from '../assets/images/cat.jpg';
+const sliderElement = document.getElementById('slider');
+let moviesData;
 
-// const img = document.createElement('img')
-// img.src = catImage
+const fetchData = async url => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
 
-sayHello();
+const printSlider = async () => {
+  const data = await fetchData(
+    'https://api.themoviedb.org/3/trending/all/day?api_key=39ecfaa61f1cb93c9e39fc6adf769ace'
+  );
+  //sliderElement.innerHTML = '';
+  const fragment = document.createDocumentFragment();
+  data.results.forEach(movie => {
+    const newMovie = document.createElement('div');
+    newMovie.classList.add('section__item');
+    const newBookmarkIcon = document.createElement('img');
+    newBookmarkIcon.classList.add('section__bookmark-icon');
+    newBookmarkIcon.src = 'assets/icon-bookmark-empty.svg';
+    const newMovieData = document.createElement('div');
+    newMovieData.classList.add('section__data');
+    const newMovieYear = document.createElement('div');
+    newMovieYear.classList.add('section__year');
+    console.log(movie.release_date);
+  });
+};
+
+printSlider();
