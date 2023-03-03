0e02624f8c432d131314929cdf80dbe112a44d8a
Elisa <elisadiazartcontact@gmail.com>
2023-03-03 18:51:25 +0100

    03.03.23

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@
 import '../scss/styles.scss';
 
 const sliderElement = document.getElementById('slider');
-const popularSection = document.getElementById('popular');
+let mediaFavoritesArray = [];
 
 const fetchData = async url => {
   const response = await fetch(url);
@@ -15,7 +15,11 @@ const API_KEY = '?api_key=39ecfaa61f1cb93c9e39fc6adf769ace';
 
 const URLS = {
   popularMovies: API_URL + '/movie/popular' + API_KEY,
-  trendingAll: API_URL + '/trending/all/day' + API_KEY
+  trendingAll: API_URL + '/trending/all/day' + API_KEY,
+  nowPlaying: API_URL + '/movie/now_playing' + API_KEY,
+  upcomingMovies: API_URL + '/movie/upcoming' + API_KEY,
+  topRatedMovied: API_URL + '/movie/top_rated' + API_KEY,
+  popularTv: API_URL + '/tv/popular' + API_KEY
 };
 
 const printSlider = async () => {
@@ -25,6 +29,11 @@ const printSlider = async () => {
     const newMovie = document.createElement('div');
     newMovie.classList.add('section__item--slider');
     newMovie.style.backgroundImage = `url('https://image.tmdb.org/t/p/w500${movie.backdrop_path}')`;
+    const newBookmarkIconContainer = document.createElement('div');
+    newBookmarkIconContainer.classList.add(
+      'section__bookmark-icon-container--slider'
+    );
+    newBookmarkIconContainer.dataset.id = movie.id;
     const newBookmarkIcon = document.createElement('img');
     newBookmarkIcon.classList.add('section__bookmark-icon--slider');
     newBookmarkIcon.src = 'assets/icon-bookmark-empty.svg';
@@ -50,7 +59,8 @@ const printSlider = async () => {
     newItemType.append(newMediaIcon);
     newMovieData.append(newMovieYear);
     newMovieData.append(newItemType);
-    newMovie.append(newBookmarkIcon);
+    newBookmarkIconContainer.append(newBookmarkIcon);
+    newMovie.append(newBookmarkIconContainer);
     newMovie.append(newMovieData);
     newMovie.append(newTitle);
     fragment.append(newMovie);
@@ -58,12 +68,11 @@ const printSlider = async () => {
   sliderElement.append(fragment);
 };
 
-const printSection = async url => {
+const printSection = async (url, id) => {
   const data = await fetchData(url);
   const fragment = document.createDocumentFragment();
   for (let index = 0; index <= 5; index++) {
     const movie = data.results[index];
-    console.log(movie);
 
     const newMovieContainer = document.createElement('div');
     newMovieContainer.classList.add('section__item__container');
@@ -72,22 +81,35 @@ const printSection = async url => {
     newMovie.classList.add('section__item');
     newMovie.style.backgroundImage = `url('https://image.tmdb.org/t/p/w500${movie.backdrop_path}')`;
 
+    const newBookmarkIconContainer = document.createElement('div');
+    newBookmarkIconContainer.classList.add('section__bookmark-icon-container');
+    newBookmarkIconContainer.dataset.id = movie.id;
+    const newBookmarkIcon = document.createElement('img');
+    newBookmarkIcon.classList.add('section__bookmark-icon');
+    newBookmarkIcon.src = 'assets/icon-bookmark-empty.svg';
+
     const newMovieData = document.createElement('div');
     newMovieData.classList.add('section__data');
 
     const newMovieYear = document.createElement('div');
     newMovieYear.classList.add('section__year');
-    newMovieYear.textContent = movie.release_date.substring(0, 4);
+    if (movie.release_date)
+      newMovieYear.textContent = movie.release_date.substring(0, 4);
+    else newMovieYear.textContent = movie.first_air_date.substring(0, 4);
 
     const newItemType = document.createElement('div');
     newItemType.classList.add('section__item-type');
-    newItemType.textContent = 'Movie';
+    if (movie.media_type === 'movie') newItemType.textContent = 'Movie';
+    else newItemType.textContent = 'Series';
 
     const newMediaIcon = document.createElement('img');
-    newMediaIcon.src = 'assets/icon-category-movie.svg';
+    if (movie.media_type === 'movie')
+      newMediaIcon.src = 'assets/icon-category-movie.svg';
+    else newMediaIcon.src = 'assets/icon-category-tv.svg';
 
     const newTitle = document.createElement('div');
-    newTitle.textContent = movie.original_title;
+    if (movie.name) newTitle.textContent = movie.name;
+    else newTitle.textContent = movie.original_title;
     newTitle.classList.add('section__item-title');
 
     if (index === 4) {
@@ -106,13 +128,29 @@ const printSection = async url => {
     newMovieData.append(newMovieYear);
     newMovieData.append(newMediaIcon);
     newMovieData.append(newItemType);
+    newBookmarkIconContainer.append(newBookmarkIcon);
+    newMovieContainer.append(newBookmarkIconContainer);
     newMovieContainer.append(newMovieData);
     newMovieContainer.append(newTitle);
     fragment.append(newMovieContainer);
   }
-  popularSection.append(fragment);
+  const Section = document.getElementById(id);
+  Section.append(fragment);
 };
 
-printSection(URLS.popularMovies);
-
+printSection(URLS.popularMovies, 'popular');
+printSection(URLS.nowPlaying, 'nowPlaying');
+printSection(URLS.upcomingMovies, 'upcoming');
+printSection(URLS.topRatedMovied, 'topRated');
+printSection(URLS.popularTv, 'popularTv');
 printSlider();
+
+document.body.addEventListener('click', e => {
+  console.log(e.target);
+  if (
+    e.target.classList.contains('section__bookmark-icon-container--slider') ||
+    e.target.classList.contains('section__bookmark-icon-container')
+  )
+    mediaFavoritesArray.push(e.target.dataset.id);
+  console.log(mediaFavoritesArray);
+});
